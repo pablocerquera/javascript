38403fd33be8e7a1b9704022610bc5ae05ac5ac4
Winston Wolfe <pabz1038@gmail.com>
2023-02-04 10:45:17 -0700

    Added some corrections to exercise 14 from the Objects chapter.

--- Objects/ex14.js
+++ Objects/ex14.js
@@ -10,7 +10,7 @@ const howdy = hello('Hi', 'Grace');
 let foo = xyzzy();
 
 
-// variables: greeting, name, howdy, Hi, Grace, foo, hello, xyzzy
+// variables: greeting, name, howdy, foo, hello, xyzzy
 // object property: a,b,c,d,0,1,2,3,4,5
 // primitive: a, b, c, d, 1, 2, 3, 4, 5, ' ', 0, 1, 2, 'Hi', 'Grace'
 // objects: { a: 1, b: 2, c: [3, 4, 5], d: {} }, [3, 4, 5], {}
